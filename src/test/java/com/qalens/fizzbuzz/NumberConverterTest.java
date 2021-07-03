8d236e19e1405b196127572788037dd9efb524c8
Naik <atm@Naiks-MacBook-Air.local>
2021-07-04 01:24:45 +0530

    ATM | corrected Tests

--- src/test/java/com/qalens/fizzbuzz/NumberConverterTest.java
+++ src/test/java/com/qalens/fizzbuzz/NumberConverterTest.java
@@ -6,21 +6,21 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class NumberConverterTest {
     @ParameterizedTest
-    @CsvSource({"1,1", "48,48", "98,98"})
+    @CsvSource({"1,1", "47,47", "98,98"})
     public void shouldConvertNumberToStringWithoutFizzOrBuzz(int number,String toString){
         final NumberConverter converter = new NumberConverter(number);
-        Assertions.assertEquals(converter.toString(),toString);
+        Assertions.assertEquals(toString,converter.toString());
     }
     @ParameterizedTest
     @CsvSource({"3,Fizz", "99,Fizz"})
     public void shouldConvertNumberToFizzWhenDivisibleBy3(int number,String toString){
         final NumberConverter converter = new NumberConverter(number);
-        Assertions.assertEquals(converter.toString(),toString);
+        Assertions.assertEquals(toString,converter.toString());
     }
     @ParameterizedTest
-    @CsvSource({"5,Buz", "100,Buzz"})
+    @CsvSource({"5,Buzz", "100,Buzz"})
     public void shouldConvertNumberToBuzzWhenDivisibleBy5(int number,String toString){
         final NumberConverter converter = new NumberConverter(number);
-        Assertions.assertEquals(converter.toString(),toString);
+        Assertions.assertEquals(toString,converter.toString());
     }
 }
