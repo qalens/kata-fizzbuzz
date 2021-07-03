80eab86d5c4ea218c5d3ee2a773a3c533e19041e
Naik <atm@Naiks-MacBook-Air.local>
2021-07-04 01:26:48 +0530

    ATM | added login for Buzz and refactored

--- src/main/java/com/qalens/fizzbuzz/FizzBuzzPrinter.java
+++ src/main/java/com/qalens/fizzbuzz/FizzBuzzPrinter.java
@@ -2,6 +2,8 @@ package com.qalens.fizzbuzz;
 
 public class FizzBuzzPrinter {
     public static void main(String[] args){
-
+        for(int i = 1;i<=100;i++){
+            System.out.println(new NumberConverter(i));
+        }
     }
 }
--- src/main/java/com/qalens/fizzbuzz/NumberConverter.java
+++ src/main/java/com/qalens/fizzbuzz/NumberConverter.java
@@ -1,6 +1,33 @@
 package com.qalens.fizzbuzz;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 public class NumberConverter {
+    private static Supplier<Stream<SpeakingNumber>> streamSupplier
+            = () -> Stream.of(new SpeakingNumber(3,"Fizz"),new SpeakingNumber(5,"Buzz"));
+    static class SpeakingNumber{
+        int number;
+        String word;
+
+        public SpeakingNumber(int number, String word) {
+            this.number = number;
+            this.word = word;
+        }
+        public boolean isNumberCompatible(int number){
+            if (number % this.number == 0){
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return word;
+        }
+    }
     int number;
     public NumberConverter(int number) {
         this.number = number;
@@ -8,8 +35,11 @@ public class NumberConverter {
 
     @Override
     public String toString() {
-        if(number%3==0)
-            return "Fizz";
-        return number+"";
+        return streamSupplier.get()
+                .filter(
+                        speakingNumber -> speakingNumber.isNumberCompatible(this.number)
+                ).findFirst()
+                .map(sn->sn.toString())
+                .orElse(number+"");
     }
 }
